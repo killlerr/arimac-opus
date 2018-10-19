1419fdfbd348d121cffdfe87cea41af4512957c9
killlerr <akilajayasooriya@gmail.com>
2018-10-19 12:30:40 +0530

    Header- responsive nav bar

--- components/Header.vue
+++ components/Header.vue
@@ -1,12 +1,22 @@
 <template>
-<div>
-<b-navbar toggleable="md" type="dark" variant="dark" class="navbar fixed-top">
+  <div>
+    <b-navbar toggleable="md" type="dark" variant="dark" class="header py-2">
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+      <b-navbar-brand href="homeUser"><img src="logo-opus-nav.jpg" class="logo-opus ml-3" alt="Responsive image"> </b-navbar-brand>
+      
+      
+      <b-collapse is-nav id="nav_collapse">
+      <b-nav-form class="search-form mx-5">
+        <b-form-input  id="input-large" size="sm" class="input-search m-2" type="text" placeholder="Search Cards" style="font-family:Arial, FontAwesome" />
+      </b-nav-form>
 
-  <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
 
-  <b-navbar-brand href="homeUser"><img src="logo-opus-nav.jpg" class="logo-opus ml-3 my-3" alt="Responsive image"> </b-navbar-brand>
-    <b-navbar-nav class="ml-auto 	">
-          <div class="container d-flex justify-content-center d-none d-sm-block d-md-none">
+      </b-collapse>
+      <!-- Right aligned nav items -->
+    <b-navbar-nav class="mx-2">
+
+<!-- profile -->
+        <div class="container d-flex justify-content-center d-none">
         <b-nav-item href="profile">
         <div class="row no-gutters">
           <div class="col-6">
@@ -17,109 +27,75 @@
             <div class="text-secondary mx-2 user-power">{{userPower}}</div>
           </div>
         </div>
-        </b-nav-item>
-          </div>
+        </b-nav-item>         
+        </div> 
+<!-- profile -->
+          <b-collapse is-nav id="nav_collapse">
+          <b-nav-item href="messageArea" class=""><fa :icon="fas.faComments"  :style="{ color: '' }" class="fa-lg m-3"/></b-nav-item>
+
+          <b-nav-item-dropdown right>
+            <!-- Using button-content slot -->
+            <template slot="button-content">
+              <em class=""><fa :icon="fas.faEllipsisV"  :style="{ color: '' }" class="fa-lg m-3 justify-content-center" /></em>
+            </template>
+            <b-dropdown-item href="settings">Settings</b-dropdown-item>
+            <b-dropdown-item href="/">Signout</b-dropdown-item>
+          </b-nav-item-dropdown>
+          </b-collapse>
     </b-navbar-nav>
-  <b-collapse is-nav id="nav_collapse">
+    
 
-      <b-nav-form class="search-form mx-5">
-        <b-form-input  id="input-large" size="sm" class="mr-sm-2" type="text" placeholder="Search Cards" style="font-family:Arial, FontAwesome" />
-      </b-nav-form>
 
-    <!-- Right aligned nav items -->
-    <b-navbar-nav class="ml-auto mx-2 my-3 ">
-
-          
-          <b-nav-item href="messageArea" class="mx-3"><fa :icon="fas.faComments"  :style="{ color: '' }" class="fa-2x mt-2"/></b-nav-item>
-
-      <b-nav-item-dropdown right>
-        <!-- Using button-content slot -->
-        <template slot="button-content">
-          <em class="mx-3"><fa :icon="fas.faEllipsisV"  :style="{ color: '' }" class="fa-2x mt-2 mr-4" /></em>
-          
-        </template>
-        <b-dropdown-item href="settings">Settings</b-dropdown-item>
-        <b-dropdown-item href="/">Signout</b-dropdown-item>
-      </b-nav-item-dropdown>
-    </b-navbar-nav>
-
-  </b-collapse>
-</b-navbar>
-
-   
-
-</div>
+    </b-navbar>
+  </div>
 </template>
 
 <script>
-
-// fontawesome: {
-//   component: 'fa'
-// }
-
-import { fas } from '@fortawesome/free-solid-svg-icons'
-  import { faGithub } from '@fortawesome/free-brands-svg-icons'
-  export default {
-    data(){
-      return{
-        userName:'Indika',
-        userPower: 'User'
-      }
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+export default {
+  data() {
+    return {
+      userName: "Iikai",
+      userPower: "User"
+    };
+  },
+  computed: {
+    fas() {
+      return fas;
     },
-    computed: {
-      fas () {
-         return fas
-      },
-      faGithub () {
-         return faGithub
-      }
-    },
+    faGithub() {
+      return faGithub;
+    }
   }
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
 
-
-
-
-
-
-.navbar{
+<style>
+.header {
   padding: 0;
   padding-top: 0.1rem;
   background-color: #2a2e43 !important;
-  width: 100vh;
-  max-width: 100%;
-  min-width: 100%;
+  width: 100%;
 }
 
-.logo-opus{
-      height: 2.8rem;
-      width: auto;    
+.logo-opus {
+  height: 2.8rem;
+  width: auto;
 }
 
-.img-profile{
-      height: 3rem;
-      width: auto;   
-      align-content: center;
-      border-radius: 50%;
-      margin-bottom: 5px;
-  
+.img-profile {
+  height: 3rem;
+  width: auto;
+  align-content: center;
+  border-radius: 50%;
+  margin-bottom: 5px;
 }
 
-.fa-size{
-      height: 1.6rem;
-      width: auto; 
-}
+/* Input */
 
-.dropdown-toggle::after {
-    display:none;
-}
-
-/* Placeholder */
-
-.mr-sm-2{
+.input-search{
     font-size: 16px;
     padding-right: 4rem;
     padding-top: 1.25rem;
@@ -128,37 +104,28 @@ import { fas } from '@fortawesome/free-solid-svg-icons'
     font-variant-position: normal;
     display: inline-block;
     background-color: #5d6177;
-    color: #9f9f9f;
     border-style: none;
     border-radius: 0;
     vertical-align: top;
 }
 
-.mr-sm-2::-webkit-input-placeholder { /* Chrome/Opera/Safari */
-    color: #afafaf;
-    opacity: 0.8;
-    margin-left: 5rem;
-}
-.mr-sm-2::-moz-placeholder { /* Firefox 19+ */
-    color: #afafaf;
-    opacity: 0.8;
-}
-.mr-sm-2:-ms-input-placeholder { /* IE 10+ */
-    color: #afafaf;
-    opacity: 0.8;
-}
-.mr-sm-2:-moz-placeholder { /* Firefox 18- */
-    color: #afafaf;
-    opacity: 0.8;
+/* .input-search:forcus, .input-search:active{
+  background-color: #5d6177;
+} */
+
+/* Input */
+
+
+/* Icons */
+.dropdown-toggle::after {
+    display:none;
 }
 
-.search-form input[type=text]{
-  padding-left: 2rem;
-  font-family: FontAwesome;
-}
 
-/* Placeholder */
+/* Icons */
 
+
+/* Profile details */
 
 .user-name{
   font-size: 16px;
@@ -179,5 +146,14 @@ import { fas } from '@fortawesome/free-solid-svg-icons'
 .nav-link{
   padding: 0;
 }
+
+/* Profile details */
+
+
+.navbar-dark .navbar-toggler {
+    border-width: 0;
+}
+
+
 
 </style>
