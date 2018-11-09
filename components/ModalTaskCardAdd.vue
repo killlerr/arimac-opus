45f4a8a5e3037decc20183f058faae0d1e071b50
killlerr <akilajayasooriya@gmail.com>
2018-11-09 21:34:17 +0530

    ModalTaskCard- DatePicker

--- components/ModalTaskCardAdd.vue
+++ components/ModalTaskCardAdd.vue
@@ -21,22 +21,26 @@
                     <div class="p-4">
                     <form @submit.stop.prevent="handleSubmit">
                         <div class="py-2">
-                            <label for="task-name">Task Name</label>
+                            <label for="task-name"><small>Name</small></label>
                             <b-form-input type="text"
                             placeholder="Add task name here"
                             id="task-name"></b-form-input>
                         </div>
                         <div class="py-2">
-                            <label for="task-name2">Project Name</label>
-                            <b-form-input type="text"
+                            <!-- input-select -->
+                            <label for="task-name2"><small>Project Name</small></label>
+                            <!-- <b-form-input type="text"
                             placeholder="Add task name here"
-                            id="task-name2"></b-form-input>
+                            id="task-name2"></b-form-input> -->
+                            <cool-select
+
+                            />
                         </div>
                         <div class="py-2">
-                            <label for="task-name3">Due date</label>
-                            <b-form-input type="text"
+                            <label for="task-name3"><small>Due date</small></label><br>
+                            <!-- <b-form-input type="text"
                             placeholder="Add task name here"
-                            id="task-name3"></b-form-input>
+                            id="task-name3"></b-form-input> --><date-picker v-model="time1" :first-day-of-week="1" lang="en"></date-picker>
                         </div>   
                     </form>
                         <div class="pt-4 pb-5">
@@ -55,9 +59,34 @@
 
 <script>
 import ReusableButton from '~/components/ReusableButton.vue'
-export default {
+import DatePicker from 'vue2-datepicker'
+import { CoolSelect } from 'vue-cool-select'
+
+export default {   
+    data() {
+        return {
+        time1: '',
+        time2: '',
+        time3: '',
+        shortcuts: [
+            {
+            text: 'Today',
+            onClick: () => {
+                this.time3 = [ new Date(), new Date() ]
+            }
+            }
+        ],
+        timePickerOptions:{
+            start: '00:00',
+            step: '00:30',
+            end: '23:30'
+        }
+        }
+    },
     components:{
         ReusableButton,
+        DatePicker,
+        CoolSelect
     },
     methods: {
         doSomething: function() {
@@ -85,14 +114,35 @@ export default {
     /*What everth width of the image it fits into the view-port*/
     background-image: url(/img-login.jpg);
     border: 0;
+    border-radius: 0 1rem 1rem 0;
 }
 
 .p-0 .modal-body {
     padding: 0;
+    border-radius: 1rem;
 }
+
+.p-0 .modal-content {
+    border-radius: 1rem;
+}
+
+
 
 .btn-add-card{
     background-color: #e74132;
     color:#fff;   
 }
+
+
+/* Date-picker */
+
+.mx-input{
+    width: 100%;
+}
+
+.mx-datepicker{
+    width: 100%;
+}
+
+/* Date-picker */
 </style>
