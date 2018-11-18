86e2237a47169147df74b525ccbfe2e621144666
killlerr <akilajayasooriya@gmail.com>
2018-11-18 20:05:40 +0530

    input-opus-styling

--- components/ReusableInput.vue
+++ components/ReusableInput.vue
@@ -32,12 +32,37 @@ export default {
     padding: 1.5rem;
     font-size: 11px;
     letter-spacing: 1px;
+    border-left: 0 solid #ff5a4e;
+    animation: opus-input-out 0.2s ease;
+
 
 }
 
 .input-reusable:focus{
     border-left: 0.4rem solid #ff5a4e;
     outline: 0;
+    /* transition: border 0.2s ease-in-out; */
+    animation: opus-input 0.2s ease;
+}
+
+@keyframes opus-input{
+    0%{
+        border-left-width: 0;
+    }
+    100%{
+        border-left-width: 0.4rem;
+
+    }
+}
+
+@keyframes opus-input-out{
+    100%{
+        border-left-width: 0;
+    }
+    0%{
+        border-left-width: 0.4rem;
+
+    }
 }
 
 </style>
